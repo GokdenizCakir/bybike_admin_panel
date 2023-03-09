9e9461fec3800ab56c38f30807715bda40cdcc6c
GokdenizCakir <gokdeniz.cakir@std.yildiz.edu.tr>
2023-03-09 16:33:40 +0300

    add bike faulty

--- src/pages/MaintenanceMan.js
+++ src/pages/MaintenanceMan.js
@@ -2,16 +2,11 @@ import React, { useEffect, useState, useRef } from 'react';
 import settings from './../assets/settings.svg';
 import notification from './../assets/notification.svg';
 import { sideState } from '../globalStates/atom';
-import {
-  MapContainer,
-  Marker,
-  Popup,
-  TileLayer,
-  useMapEvents,
-} from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import target from './../assets/target.svg';
+import bikeLogo from './../assets/bikeWhite.svg';
 import { useRecoilState } from 'recoil';
 
 const MaintenanceMan = () => {
@@ -22,66 +17,63 @@ const MaintenanceMan = () => {
   const centerRef = useRef();
   const parentBikesRef = useRef();
 
-  const renderedBikes = [
-    {
-      id: '0',
-      latitude: 41.088686794249774,
-      longitude: 28.979958520573042,
-    },
-    {
-      id: '1',
-      latitude: 41.09040376781982,
-      longitude: 28.979976653784878,
-    },
-    {
-      id: '2',
-      latitude: 41.09079424550636,
-      longitude: 28.982201439619224,
-    },
-    {
-      id: '3',
-      latitude: 41.09040376781982,
-      longitude: 28.979976653784878,
-    },
-    {
-      id: '4',
-      latitude: 41.09079424550636,
-      longitude: 28.982201439619224,
-    },
-    {
-      id: '5',
-      latitude: 41.09040376781982,
-      longitude: 28.979976653784878,
-    },
-    {
-      id: '6',
-      latitude: 41.09079424550636,
-      longitude: 28.982201439619224,
-    },
-  ];
   const bikes = [
     {
       id: '0',
       latitude: 41.088686794249774,
       longitude: 28.979958520573042,
+      charge: 20,
+      faultCode: 23442,
     },
     {
       id: '1',
       latitude: 41.09040376781982,
       longitude: 28.979976653784878,
+      charge: 46,
+      faultCode: null,
     },
     {
       id: '2',
       latitude: 41.09079424550636,
       longitude: 28.982201439619224,
+      charge: 74,
+      faultCode: 34252,
+    },
+    {
+      id: '3',
+      latitude: 41.08534361207642,
+      longitude: 28.978598650157554,
+      charge: 24,
+      faultCode: null,
+    },
+    {
+      id: '4',
+      latitude: 41.097024855859296,
+      longitude: 28.981529761373974,
+      charge: 100,
+      faultCode: null,
     },
   ];
 
-  const handleBikeSelect = (e) => {
-    setSelectedBike(e.target.options.id);
-    const childs = parentBikesRef.current.children;
-    for (let child of childs) {
-      if (child.id === e.target.options.id) {
+  const [renderedBikes, setRenderedBikes] = useState(bikes);
+
+  const renderBikes = (latitude1, longitude1, latitude2, longitude2) => {
+    const newList = bikes.filter(
+      (bike) =>
+        latitude1 < bike.latitude &&
+        bike.latitude < latitude2 &&
+        longitude1 < bike.longitude &&
+        bike.longitude < longitude2
+    );
+    setRenderedBikes(newList);
+  };
+
+  const handleBikeSelect = (id) => {
+    const { children } = parentBikesRef.current;
+
+    setSelectedBike(id);
+    for (let child of children) {
+      if (child.id === id) {
         child.scrollIntoView({
           behavior: 'smooth',
           block: 'center',
@@ -94,10 +86,28 @@ const MaintenanceMan = () => {
   const Events = () => {
     const map = useMapEvents({
       dragend() {
-        console.log('end');
+        const { _southWest, _northEast } = map.getBounds();
+        renderBikes(
+          _southWest.lat,
+          _southWest.lng,
+          _northEast.lat,
+          _northEast.lng
+        );
+        if (!renderedBikes.find((bike) => bike.id === selectedBike)) {
+          setSelectedBike('');
+        }
       },
       zoomend() {
-        console.log(map.getBounds());
+        const { _southWest, _northEast } = map.getBounds();
+        renderBikes(
+          _southWest.lat,
+          _southWest.lng,
+          _northEast.lat,
+          _northEast.lng
+        );
+        if (!renderedBikes.find((bike) => bike.id === selectedBike)) {
+          setSelectedBike('');
+        }
       },
     });
 
@@ -106,12 +116,32 @@ const MaintenanceMan = () => {
     });
   };
 
-  const bikeIcon = new L.Icon({
-    iconUrl: require('./../assets/bikeBlue.png'),
+  const selectedDefectiveBikeIcon = new L.divIcon({
+    className: 'flex justify-center items-center',
     iconSize: [36, 36],
+    html: `<div class='w-14 scale-125 h-14 p-2 transition-all rounded-[50%] overflow-hidden border-gray-400 border-2 shadow-[0_5px_50px_0px_rgba(0,0,0,0.12)] flex justify-center items-center bg-[#ff5353]'><img class='overflow-hidden' src=${bikeLogo} /></div>`,
+  });
+
+  const defectiveBikeIcon = new L.divIcon({
+    className: 'flex justify-center items-center',
+    iconSize: [36, 36],
+    html: `<div class='w-14 h-14 p-2 transition-all rounded-[50%] overflow-hidden border-gray-400 border-2 shadow-[0_5px_50px_0px_rgba(0,0,0,0.12)] flex justify-center items-center bg-[#ff5353]'><img class='overflow-hidden' src=${bikeLogo} /></div>`,
+  });
+
+  const selectedBikeIcon = new L.divIcon({
+    className: 'flex justify-center items-center',
+    iconSize: [36, 36],
+    html: `<div class='w-14 scale-125 h-14 p-2 transition-all rounded-[50%] overflow-hidden border-gray-400 border-2 shadow-[0_5px_50px_0px_rgba(0,0,0,0.12)] flex justify-center items-center bg-[#8ae2c8]'><img class='overflow-hidden' src=${bikeLogo} /></div>`,
+  });
+
+  const bikeIcon = new L.divIcon({
+    className: 'flex justify-center items-center',
+    iconSize: [36, 36],
+    html: `<div class='w-14 h-14 p-2 transition-all rounded-[50%] overflow-hidden border-gray-400 border-2 shadow-[0_5px_50px_0px_rgba(0,0,0,0.12)] flex justify-center items-center bg-[#8ae2c8]'><img class='overflow-hidden' src=${bikeLogo} /></div>`,
   });
 
   const markerIcon = new L.Icon({
+    className: 'pointer-events-none',
     iconUrl: require('./../assets/location.png'),
     iconSize: [36, 36],
     iconAnchor: [18, 36],
@@ -219,17 +249,25 @@ const MaintenanceMan = () => {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               // attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
             />
-            {bikes.map((bike, index) => (
+            {renderedBikes.map((bike, index) => (
               <Marker
                 key={index}
-                id={bike.id}
-                eventHandlers={{ click: (e) => handleBikeSelect(e) }}
-                icon={bikeIcon}
+                zIndexOffset={bike.id === selectedBike ? 100 : 0}
+                eventHandlers={{ click: () => handleBikeSelect(bike.id) }}
+                icon={
+                  bike.faultCode
+                    ? bike.id === selectedBike
+                      ? selectedDefectiveBikeIcon
+                      : defectiveBikeIcon
+                    : bike.id === selectedBike
+                    ? selectedBikeIcon
+                    : bikeIcon
+                }
                 position={[bike.latitude, bike.longitude]}
               />
             ))}
             <Events />
-            <Marker zIndexOffset={10} icon={markerIcon} position={center} />
+            <Marker zIndexOffset={1000} icon={markerIcon} position={center} />
           </MapContainer>
         ) : (
           <div className='w-full h-[60vh] lg:h-screen flex justify-center items-center '>
@@ -256,12 +294,19 @@ const MaintenanceMan = () => {
                 key={index}
                 id={bike.id}
                 className={`${
-                  selectedBike === bike.id
-                    ? 'bg-[#8ae2df] border-[#8ae2c8] h-52'
-                    : 'border-gray-300 bg-white'
-                } min-w-80 h-40 p-2 transition-all duration-300 border-2 cursor-pointer rounded-2xl shadow-[0_5px_50px_0px_rgba(0,0,0,0.12)]`}
+                  bike.faultCode
+                    ? selectedBike === bike.id
+                      ? 'bg-[#ff5353] border-[##ff5353] h-52'
+                      : 'border-gray-300 cursor-pointer bg-white'
+                    : selectedBike === bike.id
+                    ? 'bg-[#bdf1e2] border-[#bdf1e2] h-52'
+                    : 'border-gray-300 cursor-pointer bg-white'
+                } h-40 p-2 transition-all bg-[${bikeLogo}] duration-300 border-2 rounded-2xl shadow-[0_5px_10px_0px_rgba(0,0,0,0.12)]`}
               >
-                {bike.id} akdhbfakehfaıylefhjnalıeyfhanelıfhaeılufhlae
+                <div className='flex w-80 px-2 items-center pointer-events-none justify-between'>
+                  <img className='w-10' src={bikeLogo} alt='bike' />
+                  {bike.id}
+                </div>
               </div>
             ))}
           </div>
